78398556fd94576ffe2efbf83190fef53d766262
Alex Perkins <acperkins@gmail.com>
2016-03-24 08:39:17 -0400

    Ace scoring working properly.

--- blackjack.js
+++ blackjack.js
@@ -16,26 +16,43 @@
     function checkResult(standing) {
         var splitCards = display.innerHTML.split(' ');
         var cardValue = 0;
+        var hardValue = 0;
+        var ace = false;
 
         splitCards.forEach(function (card, i) {
             if (Number(card)) {
                 cardValue = cardValue + Number(card);
+                hardValue = hardValue + Number(card);
             }
             if (card === 'J' || card === 'Q' || card === 'K') {
                 cardValue = cardValue + 10;
+                hardValue = hardValue + 10;
             }
             if (card === 'A') {
-                cardValue = cardValue + 11;
+              if (!ace) {
+                hardValue = cardValue + 10;
+                ace: true;
+              }
+                cardValue = cardValue + 1;
+                hardValue = hardValue + 1;
             }
         });
-
-        if (cardValue <= 15 && standing) {
+        if (hardValue > 21) {
+          hardValue = cardValue;
+        }
+        if (hardValue <= 15 && standing) {
+            alert('Dealer wins.');
+        }
+        else if (hardValue <= 18 && standing) {
+            alert('Push!');
+        }
+        else if (cardValue <= 15 && standing) {
             alert('Dealer wins.');
         }
         else if (cardValue <= 18 && standing) {
             alert('Push!');
         }
-        if (cardValue > 18 && standing || cardValue === 21) {
+        if (cardValue > 18 && standing || cardValue > 18 && standing || cardValue === 21 || hardValue === 21) {
             alert('You win!');
         }
         else if (cardValue > 21) {
